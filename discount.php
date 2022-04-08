4f4f2d6a26584c584c566245dcb7d4f7c47fdf48
Veeti Oivo <MY_NAME@example.com>
2022-04-08 11:09:41 +0300

    discount.php lisätty.

---
+++ discount.php
@@ -0,0 +1,17 @@
+<?php
+  require_once 'inc/headers.php';
+  require_once 'inc/functions.php';
+
+  try {
+    $db = openDB();
+
+    $sql = "SELECT * FROM `tuote` where alehinta>=0";
+    $query = $db->query($sql);
+    $result = $query->fetchAll();
+
+    http_response_code(200);
+    print json_encode($result);
+
+  } catch(PDOException $error) {
+    returnError($error);
+  }
