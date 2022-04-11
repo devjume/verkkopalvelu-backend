b1f19f12380cd7835a68ddd4c54d5728f7df0ac0
SAKU PENTTILÄ <sakuinenpenttila@gmail.com>
2022-04-11 11:17:22 +0300

    carouselendpoint.php

---
+++ carouselendpoint.php
@@ -0,0 +1,22 @@
+<?php
+  require_once 'inc/headers.php';
+  require_once 'inc/functions.php';
+  $id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_NUMBER_INT);
+
+  try {
+    $db = openDB();
+
+    $sql2 = "SELECT * from tuote where tuoteryhma_id=? and alehinta<=0";
+
+    $pdoStatement = $db->prepare($sql2);
+    $pdoStatement->bindParam(1,  $id);
+    $pdoStatement->execute();
+    $result = $pdoStatement->fetchAll();
+    http_response_code(200);
+    print json_encode($result);
+
+  } catch(PDOException $error) {
+    returnError($error);
+  }
+
+  
