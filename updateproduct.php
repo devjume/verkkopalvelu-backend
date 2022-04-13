7fd6dfd3bf104af640fd990e938c61c2704de451
Saku Penttilä <sakuinenpenttila@gmail.com>
2022-04-13 15:43:39 +0300

    tuotteen muokkaaminen lisätty

---
+++ updateproduct.php
@@ -0,0 +1,41 @@
+<?php
+require_once "./inc/functions.php";
+require_once "./inc/headers.php";
+
+$tuotenimi = filter_input(INPUT_POST, "tuotenimi");
+$hinta = filter_input(INPUT_POST, "hinta", FILTER_VALIDATE_INT);
+$alehinta = filter_input(INPUT_POST, "alehinta");
+$kuvaus = filter_input(INPUT_POST, "kuvaus");
+$valmistaja = filter_input(INPUT_POST, "valmistaja");
+$kuvatiedosto = filter_input(INPUT_POST, "kuvatiedosto");
+$id = filter_input(INPUT_POST, "id");
+
+
+
+try {
+  $db = openDB();
+
+  if (!$db) {
+    echo "Database connection Failed!";
+  }
+
+  $sql = "UPDATE `tuote` SET `tuotenimi`=? , `hinta`=? , `alehinta`=? , `kuvaus`=? , `valmistaja`=? ,`kuvatiedosto`=? WHERE tuote_id = ?";
+
+  $pdoStatement = $db->prepare($sql);
+  $pdoStatement->bindParam(1, $tuotenimi);
+  $pdoStatement->bindParam(2, $hinta);
+  $pdoStatement->bindParam(3, $alehinta);
+  $pdoStatement->bindParam(4, $kuvaus);
+  $pdoStatement->bindParam(5, $valmistaja);
+  $pdoStatement->bindParam(6, $kuvatiedosto);
+  $pdoStatement->bindParam(7, $id);
+  
+
+  $pdoStatement->execute();
+
+  exit();
+
+} catch (PDOException $e) {
+  returnError($e);
+}
+
